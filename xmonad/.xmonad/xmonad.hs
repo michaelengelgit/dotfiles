b5f3b02042a269f7219578e084499d3f93f08525
Michael <michael@mail.com>
2023-08-07 14:51:48 +0200

    update xmonad config

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -33,5 +33,5 @@ main = do
        ((mod4Mask, xK_f), spawn "firejail brave"),
        ((mod4Mask, xK_x), spawn "xsecurelock"),
        ((mod4Mask, xK_m), spawn "firejail thunderbird"),
-       ((mod4Mask, xK_n), spawn "firejail rssguard")
+       ((mod4Mask, xK_n), spawn "fluent_reader")
        ]
